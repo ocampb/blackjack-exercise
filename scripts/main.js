42e78847968cdaec0cc2bddd5f8a386ab8d1c790
Olivia Campbell <106823996+ocampb@users.noreply.github.com>
2022-07-04 19:04:47 -0400

    game working
    all features working except ace card

--- scripts/main.js
+++ scripts/main.js
@@ -7,6 +7,8 @@ const playerHandContainer = document.getElementById("player-hand");
 const dealButton = document.getElementById("deal-button");
 const hitButton = document.getElementById("hit-button");
 const standButton = document.getElementById("stand-button")
+const playerPointsDisplayContainer = document.getElementById("player-points")
+const dealerPointsDisplayContainer = document.getElementById("dealer-points")
 const deck = []
 const dealerHand = []
 const playerHand = []
@@ -14,118 +16,11 @@ const suits = ["hearts", "spades", "clubs", "diamonds"]
 const ranks = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
 let playerTotal = 0
 let dealerTotal = 0
-const playerCard1 = document.createElement("img")
-playerHandContainer.append(playerCard1)
-const playerCard2 = document.createElement("img")
-playerHandContainer.append(playerCard2)
-const dealerCard1 = document.createElement("img")
-dealerHandContainer.append(dealerCard1)
-const dealerCard2 = document.createElement("img")
-dealerHandContainer.append(dealerCard2)
+const playerStatus = document.createElement("p")
+const dealerStatus = document.createElement("p")
+playerPointsDisplayContainer.append(playerStatus)
+dealerPointsDisplayContainer.append(dealerStatus)
 
-// DOM MANIPULATION:
-
-const changeCardDisplayPlayer0 = function () {
-  const rank = playerHand[0].rank
-  const suit = playerHand[0].suit
-
-  if (rank === 1) {
-    playerCard1.src = `images/ace_of_${suit}.png`
-
-  } else if (rank === 11) {
-    playerCard1.src = `images/jack_of_${suit}.png`
-
-  } else if (rank === 12) {
-    playerCard1.src = `images/queen_of_${suit}.png`
-
-  } else if (rank === 13) {
-    playerCard1.src = `images/king_of_${suit}.png`
-  }
-  else {
-    playerCard1.src = `images/${rank}_of_${suit}.png`
-  }
-
-}
-
-const changeCardDisplayPlayer1 = function () {
-  const rank = playerHand[1].rank
-  const suit = playerHand[1].suit
-  if (rank === 1) {
-    playerCard2.src = `images/ace_of_${suit}.png`
-
-  } else if (rank === 11) {
-    playerCard2.src = `images/jack_of_${suit}.png`
-
-  } else if (rank === 12) {
-    playerCard2.src = `images/queen_of_${suit}.png`
-
-  } else if (rank === 13) {
-    playerCard2.src = `images/king_of_${suit}.png`
-  }
-  else {
-    playerCard2.src = `images/${rank}_of_${suit}.png`
-  }
-}
-
-const changeCardDisplayPlayer3 = function () {
-  const rank = playerHand[2].rank
-  const suit = playerHand[2].suit
-  if (rank === 1) {
-    playerCard2.src = `images/ace_of_${suit}.png`
-
-  } else if (rank === 11) {
-    playerCard2.src = `images/jack_of_${suit}.png`
-
-  } else if (rank === 12) {
-    playerCard2.src = `images/queen_of_${suit}.png`
-
-  } else if (rank === 13) {
-    playerCard2.src = `images/king_of_${suit}.png`
-  }
-  else {
-    playerCard2.src = `images/${rank}_of_${suit}.png`
-  }
-}
-
-const changeCardDisplayDealer1 = function () {
-  const rank = dealerHand[0].rank
-  const suit = dealerHand[0].suit
-  if (rank === 1) {
-    dealerCard1.src = `images/ace_of_${suit}.png`
-
-  } else if (rank === 11) {
-    dealerCard1.src = `images/jack_of_${suit}.png`
-
-  } else if (rank === 12) {
-    dealerCard1.src = `images/queen_of_${suit}.png`
-
-  } else if (rank === 13) {
-    dealerCard1.src = `images/king_of_${suit}.png`
-  }
-  else {
-    dealerCard1.src = `images/${rank}_of_${suit}.png`
-  }
-}
-
-const changeCardDisplayDealer2 = function () {
-  const rank = dealerHand[1].rank
-  const suit = dealerHand[1].suit
-  if (rank === 1) {
-    dealerCard2.src = `images/ace_of_${suit}.png`
-
-  } else if (rank === 11) {
-    dealerCard2.src = `images/jack_of_${suit}.png`
-
-  } else if (rank === 12) {
-    dealerCard2.src = `images/queen_of_${suit}.png`
-
-  } else if (rank === 13) {
-    dealerCard2.src = `images/king_of_${suit}.png`
-  }
-  else {
-    dealerCard2.src = `images/${rank}_of_${suit}.png`
-  }
-}
 
 
 // LOGIC TO CREATE DECK: 
@@ -149,6 +44,23 @@ for (let suit of suits) {
 
 console.log(deck)
 
+// LOGIC FOR CALCULATIONS 
+
+const calculatePlayerTotal = function () {
+  let sum = 0
+  for (let index = 0; index < playerHand.length; index++) {
+    sum = sum + playerHand[index].value
+  }
+  playerTotal = sum
+}
+
+const calculateDealerTotal = function () {
+  let sum = 0
+  for (let index = 0; index < dealerHand.length; index++) {
+    sum = sum + dealerHand[index].value
+  }
+  dealerTotal = sum
+}
 
 //// LOGIC FOR CLICKING BUTTONS //////
 
@@ -161,6 +73,13 @@ const dealOneCard = function () {
   const newCard = deck.splice(newCardIndex, 1)[0]
   console.log(deck)
   return newCard
+}
+
+const changePointsDisplay = function () {
+  calculatePlayerTotal()
+  calculateDealerTotal()
+  playerStatus.innerHTML = playerTotal
+  dealerStatus.innerHTML = dealerTotal
 }
 
 const changePlayerCardDisplay = function () {
@@ -192,8 +111,36 @@ const changePlayerCardDisplay = function () {
     playerHandContainer.append(playerCard)
   }
 
+}
+
+const changeDealerCardDisplay = function () {
+  dealerHandContainer.innerHTML = "";
+
+  for (let card of dealerHand) {
+    const rank = card.rank
+    const suit = card.suit
+
+    const dealerCard = document.createElement('img')
 
 
+    if (rank === 1) {
+      dealerCard.src = `images/ace_of_${suit}.png`
+
+    } else if (rank === 11) {
+      dealerCard.src = `images/jack_of_${suit}.png`
+
+    } else if (rank === 12) {
+      dealerCard.src = `images/queen_of_${suit}.png`
+
+    } else if (rank === 13) {
+      dealerCard.src = `images/king_of_${suit}.png`
+    }
+    else {
+      dealerCard.src = `images/${rank}_of_${suit}.png`
+    }
+
+    dealerHandContainer.append(dealerCard)
+  }
 
 }
 
@@ -201,26 +148,21 @@ dealButton.onclick = function () {
   const newCard1 = dealOneCard()
   console.log(newCard1)
   playerHand.push(newCard1)
-  //changeCardDisplayPlayer0()
   console.log(playerHand)
   const newCard2 = dealOneCard()
   playerHand.push(newCard2)
-  //changeCardDisplayPlayer1()
-
-  changePlayerCardDisplay()
-
-
   console.log(dealerHand)
   const newCard3 = dealOneCard()
   console.log(newCard3)
   dealerHand.push(newCard3)
-  changeCardDisplayDealer1()
   console.log(dealerHand)
   const newCard4 = dealOneCard()
   dealerHand.push(newCard4)
   console.log(dealerHand)
   console.log(playerHand)
-  changeCardDisplayDealer2()
+  changePlayerCardDisplay()
+  changeDealerCardDisplay()
+  changePointsDisplay()
 };
 
 hitButton.onclick = function () {
@@ -228,25 +170,11 @@ hitButton.onclick = function () {
   console.log(newCard1)
   playerHand.push(newCard1)
   console.log(playerHand)
-  //changeCardDisplayPlayer3()
   changePlayerCardDisplay()
-}
-
-
-const calculatePlayerTotal = function () {
-  let sum = 0
-  for (let index = 0; index < playerHand.length; index++) {
-    sum = sum + playerHand[index].value
+  changePointsDisplay()
+  if (playerTotal > 21) {
+    endOfGame()
   }
-  playerTotal = sum
-}
-
-const calculateDealerTotal = function () {
-  let sum = 0
-  for (let index = 0; index < dealerHand.length; index++) {
-    sum = sum + dealerHand[index].value
-  }
-  dealerTotal = sum
 }
 
 const dealerTurn = function () {
@@ -258,19 +186,37 @@ const dealerTurn = function () {
   }
   calculateDealerTotal()
   console.log("dealer total" + dealerTotal)
+  changeDealerCardDisplay()
+  changePointsDisplay()
 }
 
 const endOfGame = function () {
   calculateDealerTotal()
   calculatePlayerTotal()
-  if (playerTotal === dealerTotal) {
-    console.log("TIE")
-  }
-  if (playerTotal > dealerTotal) {
+  if (dealerTotal > 21) {
     console.log("PLAYER WINS")
+    playerStatus.innerHTML = `${playerTotal} PLAYER WINS`
+    dealerStatus.innerHTML = `${dealerTotal} PLAYER WINS`
   }
-  if (playerTotal < dealerTotal) {
+  else if (playerTotal > 21) {
     console.log("DEALER WINS")
+    playerStatus.innerHTML = `${playerTotal} DEALER WINS`
+    dealerStatus.innerHTML = `${dealerTotal} DEALER WINS`
+  }
+  else if (playerTotal === dealerTotal) {
+    console.log("TIE")
+    playerStatus.innerHTML = `${playerTotal} TIE`
+    dealerStatus.innerHTML = `${dealerTotal} TIE`
+  }
+  else if (playerTotal < dealerTotal) {
+    console.log("DEALER WINS")
+    playerStatus.innerHTML = `${playerTotal} DEALER WINS`
+    dealerStatus.innerHTML = `${dealerTotal} DEALER WINS`
+  }
+  else {
+    console.log("PLAYER WINS")
+    playerStatus.innerHTML = `${playerTotal} PLAYER WINS`
+    dealerStatus.innerHTML = `${dealerTotal} PLAYER WINS`
   }
 }
 
@@ -287,7 +233,6 @@ standButton.onclick = function () {
 // Make the player and dealer bust when they hit a certain amount
 // make the dealer hit until they get to certain number
 // figure out the Ace card 
-// incorporate pictures of cards into the game logic
 
 
 
---
+++ scripts/old.js
@@ -0,0 +1,113 @@
+// DOM MANIPULATION:
+
+// const playerCard1 = document.createElement("img")
+// playerHandContainer.append(playerCard1)
+// const playerCard2 = document.createElement("img")
+// playerHandContainer.append(playerCard2)
+// const dealerCard1 = document.createElement("img")
+// dealerHandContainer.append(dealerCard1)
+// const dealerCard2 = document.createElement("img")
+// dealerHandContainer.append(dealerCard2)
+
+
+// const changeCardDisplayPlayer0 = function () {
+//   const rank = playerHand[0].rank
+//   const suit = playerHand[0].suit
+
+//   if (rank === 1) {
+//     playerCard1.src = `images/ace_of_${suit}.png`
+
+//   } else if (rank === 11) {
+//     playerCard1.src = `images/jack_of_${suit}.png`
+
+//   } else if (rank === 12) {
+//     playerCard1.src = `images/queen_of_${suit}.png`
+
+//   } else if (rank === 13) {
+//     playerCard1.src = `images/king_of_${suit}.png`
+//   }
+//   else {
+//     playerCard1.src = `images/${rank}_of_${suit}.png`
+//   }
+
+// }
+
+// const changeCardDisplayPlayer1 = function () {
+//   const rank = playerHand[1].rank
+//   const suit = playerHand[1].suit
+//   if (rank === 1) {
+//     playerCard2.src = `images/ace_of_${suit}.png`
+
+//   } else if (rank === 11) {
+//     playerCard2.src = `images/jack_of_${suit}.png`
+
+//   } else if (rank === 12) {
+//     playerCard2.src = `images/queen_of_${suit}.png`
+
+//   } else if (rank === 13) {
+//     playerCard2.src = `images/king_of_${suit}.png`
+//   }
+//   else {
+//     playerCard2.src = `images/${rank}_of_${suit}.png`
+//   }
+// }
+
+// const changeCardDisplayPlayer3 = function () {
+//   const rank = playerHand[2].rank
+//   const suit = playerHand[2].suit
+//   if (rank === 1) {
+//     playerCard2.src = `images/ace_of_${suit}.png`
+
+//   } else if (rank === 11) {
+//     playerCard2.src = `images/jack_of_${suit}.png`
+
+//   } else if (rank === 12) {
+//     playerCard2.src = `images/queen_of_${suit}.png`
+
+//   } else if (rank === 13) {
+//     playerCard2.src = `images/king_of_${suit}.png`
+//   }
+//   else {
+//     playerCard2.src = `images/${rank}_of_${suit}.png`
+//   }
+// }
+
+// const changeCardDisplayDealer1 = function () {
+//   const rank = dealerHand[0].rank
+//   const suit = dealerHand[0].suit
+//   if (rank === 1) {
+//     dealerCard1.src = `images/ace_of_${suit}.png`
+
+//   } else if (rank === 11) {
+//     dealerCard1.src = `images/jack_of_${suit}.png`
+
+//   } else if (rank === 12) {
+//     dealerCard1.src = `images/queen_of_${suit}.png`
+
+//   } else if (rank === 13) {
+//     dealerCard1.src = `images/king_of_${suit}.png`
+//   }
+//   else {
+//     dealerCard1.src = `images/${rank}_of_${suit}.png`
+//   }
+// }
+
+// const changeCardDisplayDealer2 = function () {
+//   const rank = dealerHand[1].rank
+//   const suit = dealerHand[1].suit
+//   if (rank === 1) {
+//     dealerCard2.src = `images/ace_of_${suit}.png`
+
+//   } else if (rank === 11) {
+//     dealerCard2.src = `images/jack_of_${suit}.png`
+
+//   } else if (rank === 12) {
+//     dealerCard2.src = `images/queen_of_${suit}.png`
+
+//   } else if (rank === 13) {
+//     dealerCard2.src = `images/king_of_${suit}.png`
+//   }
+//   else {
+//     dealerCard2.src = `images/${rank}_of_${suit}.png`
+//   }
+// }
